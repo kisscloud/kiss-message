b50034d2573c980da2c91fcba73cd4874849c2df
qianrongli <qianrongli@uchain.one>
2018-11-09 18:43:48 +0800

    create

---
+++ client/src/main/java/com/kiss/message/KissMessageClientApplication.java
@@ -0,0 +1,13 @@
+package com.kiss.message;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KissMessageClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KissMessageClientApplication.class);
+    }
+
+}
